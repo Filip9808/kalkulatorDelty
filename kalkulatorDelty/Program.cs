c963f3d638a475f901133ef49842e791ab6801e7
Filip9808 <filipswiatek98@gmail.com>
2022-04-03 08:56:06 +0200

    Dodaj pliki projektów.

---
+++ kalkulatorDelty/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace kalkulatorDelty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            double delta;
+            Console.WriteLine("Witaj w kalkulatorze delty " +
+                              "dla równania kwadratowego ax^2 + bx + c");
+            Console.Write("Podaj a= ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj b= ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj c= ");
+            c = Convert.ToInt32(Console.ReadLine());
+            delta = (Math.Pow(b, 2)) - 4 * a * c;
+            Console.WriteLine("Delta wynosi= {0}",delta );
+            Console.ReadKey();
+        }
+    }
+}
